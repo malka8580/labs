819f659f86d6d65b3abacf1c0125826f21793038
malka8580 <162886834+malka8580@users.noreply.github.com>
2025-01-08 12:20:52 +0200

    Update ILending.sol

--- src/Lending/ILending.sol
+++ src/Lending/ILending.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./AggregatorV3Interface.sol";
-
+import "./IUniswapV3SwapCallback.sol";
 import "./ISwapRouter.sol";
 
 
